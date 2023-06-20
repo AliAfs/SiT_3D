596927b65e214004f34fced7b7ac01d8894d2cd9
Ali Afsharian <aliafsharian.hotmail.com>
2023-06-21 00:10:49 +0200

    Add new files specific to the project

---
+++ datasets/datasets_utils_test.py
@@ -0,0 +1,249 @@
+from PIL import Image
+import numpy as np
+import random
+
+from numpy.random import randint
+import torch
+
+from PIL import ImageFilter, ImageOps
+from torchvision import transforms as tf
+
+from torchvision import transforms
+
+
+def buildLabelIndex(labels):
+    label2inds = {}
+    for idx, label in enumerate(labels):
+        if label not in label2inds:
+            label2inds[label] = []
+        label2inds[label].append(idx)
+
+    return label2inds
+
+
+def getItem(idx, X, target = None, transform=None, training_mode = 'SSL'):
+    if transform is not None:
+        X = transform(X)
+
+    return X, target
+
+
+
+class myRandCrop(tf.RandomResizedCrop):
+    def __init__(self, size, scale=(0.08, 1.0), ratio=(3. / 4., 4. / 3.), interpolation=Image.BILINEAR):
+        super(myRandCrop, self).__init__(size, scale, ratio, interpolation)
+        
+    def forward(self, img):
+        i, j, h, w = self.get_params(img, self.scale, self.ratio)
+        return tf.functional.resized_crop(img, i, j, h, w, self.size, self.interpolation), (i, j, h, w)
+   
+class myRandomHorizontalFlip(tf.RandomHorizontalFlip):
+    def __init__(self, p=0.5):
+        super(myRandomHorizontalFlip, self).__init__(p=p)
+        
+    def forward(self, img):
+        if torch.rand(1) < self.p:
+            return tf.functional.hflip(img), 1
+        return img, 0
+    
+class PadAndCrop(object):
+    """
+    Apply Padding and Cropping to the 3D tensor.
+    """
+    def __init__(self, output_size=(147, 224, 224)):
+        self.output_size = output_size
+
+    def __call__(self, tensor):
+        #tensor = torch.from_numpy(sample).float()
+        dz, dy, dx = np.subtract(self.output_size, tensor.shape)
+
+        dx_left = dx // 2
+        dx_right = dx - dx_left
+        dy_left = dy // 2
+        dy_right = dy - dy_left
+        dz_left = dz // 2
+        dz_right = dz - dz_left
+
+        tensor_resized = torch.nn.functional.pad(tensor, (dx_left, dx_right, dy_left, dy_right, dz_left, dz_right), "constant")
+        
+        return tensor_resized.unsqueeze(0)
+    
+class RandomVolumePatch(object):
+    """
+    Select a random volume patch from 3D tensor.
+    """
+    def __init__(self, threshold=50, volume_shape=(21, 64, 64), required_percentage=0.7):
+        self.threshold = threshold
+        self.volume_shape = volume_shape
+        self.required_percentage = required_percentage
+
+    def __call__(self, tensor):
+        tensor_shape = tensor.shape
+        
+        if len(tensor_shape) != 3:
+            raise ValueError("The input tensor must be a 3D tensor.")
+
+        if any(dim < size for dim, size in zip(tensor_shape, self.volume_shape)):
+            raise ValueError("The volume size cannot be larger than the tensor dimensions.")
+
+        while True:
+            # Generate random indices for the volume
+            start_indices = [np.random.randint(dim - size + 1) for dim, size in zip(tensor_shape, self.volume_shape)]
+            end_indices = [start + size for start, size in zip(start_indices, self.volume_shape)]
+
+            # Extract the random volume from the tensor
+            volume = tensor[start_indices[0]:end_indices[0], start_indices[1]:end_indices[1], start_indices[2]:end_indices[2]]
+            
+            # Calculate the percentage of values above the threshold
+            above_threshold = torch.sum(volume > self.threshold).item()
+
+            percentage_above_threshold = above_threshold / torch.numel(volume)
+
+            if percentage_above_threshold >= self.required_percentage:
+                return volume.unsqueeze(0)
+
+def GMML_replace_list(samples, corrup_prev, masks_prev, drop_type='noise', max_replace=0.35, align=(7, 16, 16)):
+        
+    rep_drop = 1 if drop_type == '' else ( 1 / ( len(drop_type.split('-')) + 1 ) )
+    
+    n_imgs = samples.size()[0] #this is batch size, but in case bad inistance happened while loading
+    samples_aug = samples.detach().clone()
+    masks = torch.zeros_like(samples_aug)
+    for i in range(n_imgs):
+        idx_rnd = randint(0, n_imgs)
+        if random.random() < rep_drop: 
+            samples_aug[i], masks[i] = GMML_drop_rand_patches_3d(samples_aug[i], samples[idx_rnd], max_replace=max_replace, align=align)
+        else:
+            samples_aug[i], masks[i] = corrup_prev[i], masks_prev[i]
+
+    return samples_aug, masks
+
+def GMML_drop_rand_patches_3d(X, X_rep=None, drop_type='noise', max_replace=0.7, align=(7, 16, 16), max_block_sz=0.4):
+    import torch
+    import numpy as np
+    from random import randint
+
+    #######################
+    # max_replace: percentage of image to be replaced
+    # align: align corruption with the patch sizes
+    # max_block_sz: percentage of the maximum block to be dropped
+    #######################
+
+    torch.manual_seed(0)
+    np.random.seed(0)
+
+    C, D, H, W = X.size()
+    n_drop_pix = np.random.uniform(min(0.5, max_replace), max_replace) * D * H * W
+    mx_blk_depth = int(D * max_block_sz)
+    mx_blk_height = int(H * max_block_sz)
+    mx_blk_width = int(W * max_block_sz)
+
+    align_d, align_h, align_w = align
+
+    #align = max(1, align)
+
+    mask = torch.zeros_like(X)
+    drop_t = np.random.choice(drop_type.split('-'))
+
+    while mask[0].sum() < n_drop_pix:
+
+        ####### get a random block to replace
+        rnd_e = (randint(0, D - align_d) // align_d) * align_d
+        rnd_r = (randint(0, H - align_h) // align_h) * align_h
+        rnd_c = (randint(0, W - align_w) // align_w) * align_w
+
+        rnd_d = min(randint(align_d, mx_blk_depth), D - rnd_e)
+        rnd_d = round(rnd_d / align_d) * align_d
+        rnd_h = min(randint(align_h, mx_blk_height), H - rnd_r)
+        rnd_h = round(rnd_h / align_h) * align_h
+        rnd_w = min(randint(align_w, mx_blk_width), W - rnd_c)
+        rnd_w = round(rnd_w / align_w) * align_w
+
+        if X_rep is not None:
+            X[:, rnd_e:rnd_e + rnd_d, rnd_r:rnd_r + rnd_h, rnd_c:rnd_c + rnd_w] = X_rep[:,
+                                                                                  rnd_e:rnd_e + rnd_d,
+                                                                                  rnd_r:rnd_r + rnd_h,
+                                                                                  rnd_c:rnd_c + rnd_w].detach().clone()
+        else:
+            if drop_t == 'noise':
+                X[:, rnd_e:rnd_e + rnd_d, rnd_r:rnd_r + rnd_h, rnd_c:rnd_c + rnd_w] = torch.empty(
+                    (C, rnd_d, rnd_h, rnd_w), dtype=X.dtype, device=X.device).normal_()
+            elif drop_t == 'zeros':
+                X[:, rnd_e:rnd_e + rnd_d, rnd_r:rnd_r + rnd_h, rnd_c:rnd_c + rnd_w] = torch.zeros(
+                    (C, rnd_d, rnd_h, rnd_w), dtype=X.dtype, device=X.device)
+            else:
+                ####### get a random block to replace from
+                rnd_e2 = (randint(0, D - rnd_d) // align_d) * align_d
+                rnd_r2 = (randint(0, H - rnd_h) // align_h) * align_h
+                rnd_c2 = (randint(0, W - rnd_w) // align_w) * align_w
+
+                X[:, rnd_e:rnd_e + rnd_d, rnd_r:rnd_r + rnd_h, rnd_c:rnd_c + rnd_w] = X[:,
+                                                                                      rnd_e2:rnd_e2 + rnd_d,
+                                                                                      rnd_r2:rnd_r2 + rnd_h,
+                                                                                      rnd_c2:rnd_c2 + rnd_w].detach().clone()
+
+        mask[:, rnd_e:rnd_e + rnd_d, rnd_r:rnd_r + rnd_h, rnd_c:rnd_c + rnd_w] = 1
+
+    return X, mask
+
+class DataAugmentationSiT(object):
+    def __init__(self, args):
+        
+        # for corruption
+        self.drop_perc = args.drop_perc
+        self.drop_type = args.drop_type
+        self.drop_align = args.drop_align
+        
+        
+        # first global crop
+        self.global_transfo1 = transforms.Compose([
+            torch.from_numpy,
+            transforms.ConvertImageDtype(torch.float32),
+            #PadAndCrop(output_size=(147, 224, 224)),
+            RandomVolumePatch(),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(degrees=10)
+        ])
+
+        # second global crop
+        self.global_transfo2 = transforms.Compose([
+            torch.from_numpy,
+            transforms.ConvertImageDtype(torch.float32),
+            #PadAndCrop(output_size=(147, 224, 224)),
+            RandomVolumePatch(),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomRotation(degrees=10)
+        ])
+
+    def __call__(self, image):
+        
+        clean_crops = []
+        corrupted_crops = []
+        masks_crops = []
+
+        ## augmented 1
+        im_orig = self.global_transfo1(image)
+        
+        im_corrupted = im_orig.detach().clone()
+        im_mask = torch.zeros_like(im_corrupted)
+        if self.drop_perc > 0:
+            im_corrupted, im_mask = GMML_drop_rand_patches_3d(im_corrupted, 
+                                                           max_replace=self.drop_perc, drop_type=self.drop_type, align=self.drop_align)
+        clean_crops.append(im_orig)
+        corrupted_crops.append(im_corrupted)
+        masks_crops.append(im_mask)
+        
+        ## augmented 2
+        im_orig = self.global_transfo2(image)
+        
+        im_corrupted = im_orig.detach().clone()
+        im_mask = torch.zeros_like(im_corrupted)
+        if self.drop_perc > 0:
+            im_corrupted, im_mask = GMML_drop_rand_patches_3d(im_corrupted, 
+                                                           max_replace=self.drop_perc, drop_type=self.drop_type, align=self.drop_align)
+        clean_crops.append(im_orig)
+        corrupted_crops.append(im_corrupted)
+        masks_crops.append(im_mask)
+        
+        return clean_crops, corrupted_crops, masks_crops
+
---
+++ datasets/load_dataset_test.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import torch
+
+def load_data_list():
+    folder_path = "../cohort_new/numpy arrays"
+    file_list = os.listdir(folder_path)
+
+    data_list = []
+    for file_name in file_list:
+        if file_name.endswith('.npy'):
+            file_path = os.path.join(folder_path, file_name)  # Construct the full file path
+            array = np.load(file_path).astype(float)          # Load the array and convert its type to float
+            data_list.append(array)
+
+    # Squeeze the arrays to remove the redundant dimension
+    data_list = [arr.squeeze() for arr in data_list]
+    
+    """
+    data_list_resized = []
+    output_size = (147, 224, 224)
+    for sample in data_list:
+        sample_tensor = torch.from_numpy(sample).float()
+        dz, dy, dx = np.subtract(output_size, sample_tensor.shape)
+
+        dx_left = dx // 2
+        dx_right = dx - dx_left
+        dy_left = dy // 2
+        dy_right = dy - dy_left
+        dz_left = dz // 2
+        dz_right = dz - dz_left
+
+        sample_resized = torch.nn.functional.pad(sample_tensor, (dx_left, dx_right, dy_left, dy_right, dz_left, dz_right), "constant")
+        data_list_resized.append(sample_resized.unsqueeze(0))
+    """
+
+    return data_list 
